82315efe4228238b7b835193a8d5f25304ce5a38
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-01 14:55:33 -0400

    Exercise 0

---
+++ exercises.rb
@@ -0,0 +1,12 @@
+#Exercise 0
+
+colours = ["black", "blue", "rainbow", "shade", "darkness", "green", "teal", "plaid"]
+ages = [24, 54, 65, 61, 32, 32, 30, 33, 36, 35]
+coin_flips = ["heads", "tails", "heads", "heads", "heads"]
+artists = ["Denzel Washington", "Nash Bridges", "Charlize Theron"]
+symbol_colours = [:black,:blue,:rainbow,:shade,:darkness,:green,:teal,:plaid]
+
+
+
+
+#Bottom
