9375edc17c0b7a25d9a6816793b625c722da37aa
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-01 15:15:11 -0400

    Exercise 0 all complete

--- exercises.rb
+++ exercises.rb
@@ -6,6 +6,14 @@ coin_flips = ["heads", "tails", "heads", "heads", "heads"]
 artists = ["Denzel Washington", "Nash Bridges", "Charlize Theron"]
 symbol_colours = [:black,:blue,:rainbow,:shade,:darkness,:green,:teal,:plaid]
 
+words = {:werlleroq => "to be hit while spinning in circles", :cognaissent => "a state of being when ones mind has fled to another realm", :fukenlorn => "when one almost achieves their goal but barely misses"}
+movie_dates = {:braveheart => 1995, :dredd => 2012, :"the dark knight" => 2008}
+city_populations = {:toronto => 3000000, :shanghai => 18000000, :johannesburg => 1000000}
+name_ages = {:billybob => 28, :susannahan => 35, :gilgamesh => 25000}
+
+
+#Exercise 1
+
 
 
 
